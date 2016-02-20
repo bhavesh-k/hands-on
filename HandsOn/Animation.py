a10d1a96196e4406aab62bea23c3d727054cf9ec
Bhavesh Kakwani <bhaveshtk@gmail.com>
2016-02-20 00:41:42 -0500

    Palm orientation and index knuckle bending works. Now we have a blueprint for setting up all the knuckles and fingers

--- HandsOn/Animation.py
+++ HandsOn/Animation.py
@@ -15,16 +15,16 @@ from direct.leveleditor.AnimGlobals import FRAME
 #global pitch, yaw, roll
 
 ## Draw the 3D hand based on parsed serial data
-def drawBox():
+def drawBox(width,height,depth):
     vertices = (
-        (1,-1,-1),
-        (1,1,-1),
-        (-1,1,-1),
-        (-1,-1,-1),
-        (1,-1,1),
-        (1,1,1),
-        (-1,-1,1),
-        (-1,1,1)
+        (width/2,-height/2,-depth/2),
+        (width/2,height/2,-depth/2),
+        (-width/2,height/2,-depth/2),
+        (-width/2,-height/2,-depth/2),
+        (width/2,-height/2,depth/2),
+        (width/2,height/2,depth/2),
+        (-width/2,-height/2,depth/2),
+        (-width/2,height/2,depth/2)
         )
 
     edges = (
@@ -49,6 +49,36 @@ def drawBox():
 
     glEnd()
 
+def drawHand(curr_pitch,curr_yaw,curr_roll):
+    # draw the 3D hand
+    
+    # rotate by pitch, yaw and roll
+    glRotatef(share_var.pitch - curr_pitch, 1, 0, 0)
+    glRotatef(share_var.yaw - curr_yaw, 0, 1, 0)
+    glRotatef(share_var.roll - curr_roll, 0, 0, 1)
+    
+    # draw the palm
+    drawBox(3,1,2.2)
+    
+    # store the current orientation for use in the next frame
+    curr_pitch = share_var.pitch
+    curr_yaw = share_var.yaw
+    curr_roll = share_var.roll
+    
+    ## draw knuckles and fingers
+    # index knuckle
+    glTranslatef(-1.7,0,-1)
+    glRotatef(-share_var.indexKnuckleDeg, 1, 0, 0)
+    glTranslatef(0,0,-1.1)
+    drawBox(0.6,1,2)
+    glTranslatef(0,0,1.1)
+    glRotatef(share_var.indexKnuckleDeg, 1, 0, 0)
+    glTranslatef(1.7,0,1)
+    
+    return curr_pitch,curr_yaw,curr_roll
+    
+    
+
 def main():
     pygame.init()
     
@@ -56,11 +86,12 @@ def main():
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
         
     # view angle, aspect ratio, z_near, z_far (z's are clipping planes)
-    gluPerspective(45, 1, 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5) # move cube away from screen (zoom out)
+    gluPerspective(90, 1, 1.0, 50.0)
+    glTranslatef(0.0, 0.0, -4) # move cube away from screen (zoom out)
     glRotatef(0, 0, 0, 0)
     
-    ser = serial.Serial('/dev/ttyACM0', 9600) #Open serial port
+    # Open serial port and parse serial input inside a thread
+    ser = serial.Serial('/dev/ttyACM0', 9600)
     serialThread = threading.Thread(target=HandsOn.parseSerialHandData, args=(ser,))
     serialThread.setDaemon(True)
     serialThread.start()
@@ -73,18 +104,14 @@ def main():
                 pygame.quit()
                 ser.close()
                 quit()
-                
-        glRotatef(share_var.pitch - curr_pitch, 1, 0, 0)
-        glRotatef(share_var.yaw - curr_yaw, 0, 1, 0)
-        glRotatef(share_var.roll - curr_roll, 0, 0, 1)
         
-        curr_pitch = share_var.pitch
-        curr_yaw = share_var.yaw
-        curr_roll = share_var.roll
+        # clear GL frame
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) # clear GL frame
+        # draw 3D hand and also update the pitch, yaw and roll
+        curr_pitch,curr_yaw,curr_roll = drawHand(curr_pitch,curr_yaw,curr_roll)
         
-        drawBox()
+        # refresh the frame
         pygame.display.flip()
         
         # pygame.time.wait(20) # wait 20ms
